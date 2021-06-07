7dd0ec46b7ad6fb7a2bfc3d598e0bc065699357f
meglenast <58610466+meglenast@users.noreply.github.com>
2021-06-07 23:14:16 +0300

    load data

---
+++ loadData.sql
@@ -0,0 +1,177 @@
+--artist
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Conchita', 'Grimsdith', 'Sweden', '1952-04-11', '1989-07-18');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Ginnifer', 'Searsby', 'Philippines', null, null);
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Corilla', 'Wolland', 'Greece', '1901-01-10', '2012-11-14');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Ase', 'Crowhurst', 'Argentina', '1935-06-01', '2010-11-20');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Mano', 'Sutherland', 'Philippines', '1972-06-28', '1992-07-31');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Vaughn', 'Doody', 'China', '1994-08-07', '2013-10-22');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Edsel', 'Favell', 'Russia', '1915-04-01', '2005-05-31');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Martie', 'Comettoi', 'Indonesia', '1985-05-06', '2000-11-06');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Cooper', 'Seary', 'China', '1932-10-26', '2008-06-18');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Werner', 'Tallent', 'China', '1921-09-28', '1980-11-04');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Laurette', 'Vooght', 'Brazil', '1901-03-25', '1973-05-23');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('June', 'Matushenko', 'China', '1947-07-12', '1981-12-21');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Craggy', 'Bulger', 'Moldova', '1902-09-10', '1949-12-03');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Elbertina', 'Bernt', 'Namibia', '1934-06-02', '2001-12-28');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Meriel', 'Parsonage', 'China', '1943-08-30', '2011-03-11');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Adria', 'Aguirrezabal', 'Thailand', '1951-05-02', '1964-12-22');
+insert into artist (first_name, last_name, country, date_of_birth, date_of_death) values ('Garrett', 'Godier', 'Mexico', null, null);
+
+--customer
+
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Kelley', 'Beed', 'ZvZfzbV4Cbv', '8377265909', 'kbeed0@ebay.com', '6253 Roth Center');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Whittaker', 'Wieprecht', 'nb4ddZNq', '2904500000', 'wwieprecht1@whitehouse.gov', '3799 Graedel Street');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Inna', 'Strickland', '80qTcC', '4394297976', 'istrickland2@redcross.org', '7 Prairie Rose Junction');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Meghan', 'Edgerly', '1c7n6pHb1', '7559580652', 'medgerly3@oakley.com', '44 Forster Point');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Coral', 'Tabard', 'UQsx5jS', '2245978818', 'ctabard4@php.net', '7429 Pond Pass');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Wilfrid', 'Gingell', '4NMeT4P', '7564593906', 'wgingell5@ask.com', '099 Troy Road');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Karlie',  'Ould', 'SyRIdD4tjZP', '9095417407', 'kould6@fotki.com', '76 Claremont Junction');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Hyacintha', 'Elstob', '9c8QedIV', '5267980923', 'helstob7@hexun.com', '0 Fairfield Trail');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Helge', 'Malletratt', 'b33cV192Q', '4192722501', 'hmalletratt8@army.mil', '14275 Sundown Pass');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Gare', 'Jerrim', 'THslfm5I9K7', '5438607267', 'gjerrim9@about.me', '1643 Sachtjen Drive');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Francisco', 'Pieter', 'XWk2eBz5', '8101864706', 'fpietera@usatoday.com', '9459 Lotheville Junction');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Kermy', 'Sinkin', 'Li2oF', '4689725652', 'ksinkinb@booking.com', '10508 Golden Leaf Circle');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Gary', 'Couch', 'XtvYKE7', '4659558553', 'gcouchc@ustream.tv', '2 Thierer Way');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Brita', 'Van Halle', 'toP4caR', '2779266603', 'bvanhalled@mapquest.com', '468 Fuller Circle');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Neille', 'McDool', '6o12U82qKDQ', '4193057128', 'nmcdoole@sourceforge.net', '854 Holy Cross Lane');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Dione', 'Shillitoe', 'A6FyuxSPG', '6787515290', 'dshillitoef@scientificamerican.com', '31 Huxley Pass');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Beryle', 'Biagioni', '4X15ouS', '7269612030', 'bbiagionih@yolasite.com', '539 Ramsey Center');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Lilyan', 'Gonet', 'GRz5H6Vl', '5963582444', 'lgoneti@elegantthemes.com', '797 Mallard Trail');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Helene', 'Leggott', 'mMk4kSR', '1482036722', 'hleggottj@devhub.com', '20919 Little Fleur Place');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Roda', 'Tourry', 'qD2eXED4', '3788535497', 'rtourryk@virginia.edu', '89 American Lane');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Maye', 'Schutte', 'pU34PoEwN', '9541110082', 'mschuttel@cpanel.net', '94 Farragut Drive');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Celle', 'Toler', 'BYlGeoK6', '7215153517', 'ctolerm@i2i.jp', '6 Merchant Point');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Jdavie', 'Trenbey', 'tM38LOUYqM', '4044081012', 'jtrenbeyn@fotki.com', '697 Onsgard Point');
+insert into customer (first_name, last_name, passwd, phone, email, address) values ('Ricki', 'Darlison', 'Xn7vKeB2', '6285731617', 'rdarlisono@globo.com', '7 Stoughton Court');
+
+--payment
+
+insert into payment (cardNo, owner_firstname, owner_lastname, expiration_date) values ('301771555836', 'Cristy', 'Kitchen', '2020-10-28');
+insert into payment (cardNo, owner_firstname, owner_lastname, expiration_date) values ('358146446840', 'Freddy', 'Moyles', '2020-06-29');
+insert into payment (cardNo, owner_firstname, owner_lastname, expiration_date) values ('560221625665', 'Othelia', 'Waterstone', '2020-12-13');
+insert into payment (cardNo, owner_firstname, owner_lastname, expiration_date) values ('304596354637', 'Honey', 'Brodie', '2021-11-15');
+insert into payment (cardNo, owner_firstname, owner_lastname, expiration_date) values ('356542895699', 'Cindee', 'Coltart', '2021-04-27');
+insert into payment (cardNo, owner_firstname, owner_lastname, expiration_date) values ('355580774073', 'Debera', 'Laxen', '2021-09-07');
+insert into payment (cardNo, owner_firstname, owner_lastname, expiration_date) values ('560222046426', 'Paquito', 'Deboo', '2021-03-26');
+
+--exhibition
+
+insert into exhibition (name_exhibition, date_starting, date_ending, price_ticket) values ('vitae ipsum aliquam', '2020-08-24', '2021-01-14', 10.02);
+insert into exhibition (name_exhibition, date_starting, date_ending, price_ticket) values ('sapien', '2020-11-10', '2020-12-10', 2.69);
+insert into exhibition (name_exhibition, date_starting, date_ending, price_ticket) values ('mattis egestas', '2021-05-27', '2021-12-13', 19.55);
+insert into exhibition (name_exhibition, date_starting, date_ending, price_ticket) values ('porttitor lacus', '2020-09-18', '2021-04-02', 33.93);
+insert into exhibition (name_exhibition, date_starting, date_ending, price_ticket) values ('luctus', '2021-01-23', '2022-07-28', 41.29);
+insert into exhibition (name_exhibition, date_starting, date_ending, price_ticket) values ('nulla eget eros', '2021-02-23', '2021-05-21', 5.74);
+insert into exhibition (name_exhibition, date_starting, date_ending, price_ticket) values ('dapibus', '2020-08-15', '2021-05-27', 52.99);
+
+--exhibition_hall
+
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1000, 1, 13);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1003, 0, 12);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1004, 2, 14);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1005, 2, 8);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1004, 0, 1);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1000, 3, 8);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1003, 1, 6);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1003, 3, 16);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1005, 0, 6);
+insert into exhibition_hall (id_exhibition, floor, capacity) values (1006, 2, 5);
+
+--ticket
+
+insert into ticket (id_exhibition, date, type) values (1000, '2020-12-28', N'adult');
+insert into ticket (id_exhibition, date, type) values (1003, '2021-01-30', N'adult');
+insert into ticket (id_exhibition, date, type) values (1005, '2021-01-09', N'adult');
+insert into ticket (id_exhibition, date, type) values (1003, '2020-07-10', N'student');
+insert into ticket (id_exhibition, date, type) values (1004, '2021-03-31', N'student');
+insert into ticket (id_exhibition, date, type) values (1006, '2021-01-17', N'student');
+insert into ticket (id_exhibition, date, type) values (1005, '2020-10-22', N'child');
+insert into ticket (id_exhibition, date, type) values (1003, '2021-03-07', N'child');
+insert into ticket (id_exhibition, date, type) values (1004, '2020-06-11', N'elder');
+insert into ticket (id_exhibition, date, type) values (1005, '2021-03-05', N'elder');
+
+--shopping_cart
+
+insert into shopping_cart (id_customer, quantity, total_price) values (1015, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1016, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1017, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1018, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1019, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1020, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1021, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1022, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1023, 0, 0);
+insert into shopping_cart (id_customer, quantity, total_price) values (1024, 0, 0);
+
+--orders
+
+insert into orders (cardNo, id_cart, ord_date, status) values ('301771555836', 1,  '2021-04-06', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('358146446840', 2,  '2020-12-17', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('560221625665', 3,  '2021-03-11', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('301771555836', 4,  '2021-01-10', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('356542895699', 5,  '2021-02-17', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('560222046426', 6,  '2021-01-20', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('560221625665', 7,  '2020-11-09', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('301771555836', 8,  '2020-11-23', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('358146446840', 9,  '2021-05-18', N'completed');
+insert into orders (cardNo, id_cart, ord_date, status) values ('560222046426', 10, '2020-08-13', N'completed');
+
+--painting
+
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1000, 1000, 'non velit donec', '1970-04-11', 86, 55.5, N'oil', 21045979.19, 43458094.75, 2, N'reproduction');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1002, 1000, 'augue a suscipit', '1965-01-10', 86, 55.5, N'oil', null, 72155798.03, 3, N'reproduction');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1003, 1002, 'vehicula consequat morbi', '1965-01-10', 86, 55.5, N'acryl', 67386132.13, 61720208.37, 6, N'print');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1004, 1003, 'aenean fermentum', '1991-07-31', 86, 55.5, N'oil', 47005917.38, 61102821.58, 3, N'reproduction');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1005, 1000, 'vulputate', null, 86, 55.5, N'acryl', 68210849.62, 36597531.20, 6, N'print');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1006, 1005, 'donec dapibus duis', null, 86, 55.5, N'acryl', 34913898.00, 6854079.27, 4, N'reproduction');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1007, 1002, 'ut at dolor quis', null, 86, 55.5, N'acryl', 6890810.86, 25855014.50, 4, N'reproduction');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1008, 1000, 'at ipsum ac tellus', null, 86, 55.5, N'sketch', 69039674.83, 33758576.76, 1, N'original');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1009, 1003, 'integer tincidunt ante vel', null, 86, 55.5, N'sketch', 41484145.02, 24774960.87, 1, N'original');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1001, 1001, 'amet lobortis', null, 86, 55.5, N'graphic', 400, 416, 2, N'reproduction');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1002, 1005, 'nunc', '1954-01-10', 86, 55.5, N'graphic', 45603792.82, 91293091.72, 6, N'print');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1003, 1003, 'eu interdum eu tincidunt', '1959-01-10', 86, 55.5, N'graphic', null, 1712789.50, 1, N'original');
+insert into painting (id_artist, id_hall, title, year_painted, width, height, drawing_technique, price_sell, price_bought, number_copies, type_painting) values 
+	(1004, 1002, 'integer aliquet massa', '1992-03-31', 86, 55.5, N'graphic', 32426983.29, 29875392.18, 6, N'original');
+
+--still_life_painting
+insert into still_life_painting (id_painting) values(100000);
+insert into still_life_painting (id_painting) values(100001);
+insert into still_life_painting (id_painting) values(100002);
+insert into still_life_painting (id_painting) values(100003);
+
+
+--landscape_painting
+insert into landscape_painting (id_artist) values(100004);
+insert into landscape_painting (id_artist) values(100005);
+insert into landscape_painting (id_artist) values(100006);
+
+--genre_painting
+insert into genre_painting (id_artist) values(100007);
+insert into genre_painting (id_artist) values(100008);
+insert into genre_painting (id_artist) values(100009);
+
+--portrait_art
+insert into portrait_art (id_artist) values(100001);
+insert into portrait_art (id_artist) values(100002);
+insert into portrait_art (id_artist) values(100003);
+
+--history_painting
+insert into history_painting (id_artist) values(100003);
+insert into history_painting (id_artist) values(100004);
+insert into history_painting (id_artist) values(100005);
+
+
+--painting_shoppingcart
+--insert into painting_shoppingcart(id_painting, id_cart) values () 
